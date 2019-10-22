ccf6908bd766161dffa4bd2b86df86d31e0348dd
Amita999 <50816989+Amita999@users.noreply.github.com>
2019-10-22 11:32:12 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-let express = require("express");
-let app = express();
+let mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost/ractiseDb", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("Connection Suuccesful"))
+  .catch(ex => console.log(`Wrong connection ${ex.message}`));
+let albumSchema = new mongoose.Schema({
+  title: { type: String },
+  singer: { type: String },
+  genre: { type: String },
+  p_date: { type: Date, default: Date.now }
+});
 
-app.use(express.json());
-let album = require("./routes/album");
-app.use("/api/allAlbums/", album);
-app.listen(4500, () => console.log("Port is working"));
+let Album = mongoose.model("albums", albumSchema, "albums");
+
+async function AddAlbums() {
+  let newAlbum = new Album({
+    title: "Esposa",
+    singer: "Seth ",
+    genre: "Romace"
+  });
+  let data = await newAlbum.save();
+  console.log(data);
+}
+
+AddAlbums();
